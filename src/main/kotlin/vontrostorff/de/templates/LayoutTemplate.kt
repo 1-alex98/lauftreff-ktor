849b1434588917bf05ca305baf6ad793d2d52b63
trostoal <alexander.vontrostorff@dormakaba.com>
2023-08-08 12:08:04 +0200

    Responsive

--- src/main/kotlin/vontrostorff/de/templates/LayoutTemplate.kt
+++ src/main/kotlin/vontrostorff/de/templates/LayoutTemplate.kt
@@ -13,6 +13,10 @@ class LayoutTemplate: Template<HTML> {
                 src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
             }
             link(href = "/static/main.css", rel = "stylesheet")
+            meta{
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+            }
         }
         body {
             div{

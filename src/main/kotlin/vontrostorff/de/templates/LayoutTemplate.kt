ba702eaa4554e39437d418d7239d90e955ca331e
alex <alexander.von.trostorff@gmail.com>
2023-08-13 22:11:58 +0200

    Home as link

--- src/main/kotlin/vontrostorff/de/templates/LayoutTemplate.kt
+++ src/main/kotlin/vontrostorff/de/templates/LayoutTemplate.kt
@@ -11,7 +11,7 @@ class LayoutTemplate: Template<HTML> {
             link(href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css", rel = "stylesheet")
             link{
                 rel="icon"
-                href="images/favicon.svg"
+                href="/static/runner.svg"
                 sizes="any"
                 type="image/svg+xml"
             }
